0c4496a962392b55164ef42f0ba029b88e12d44c
antonysa23-meet <antony.saleh2017@gmail.com>
2022-07-26 16:35:46 +0300

    Update app.py

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -9,7 +9,28 @@ app = Flask(  # Create a flask app
 
 # Your code should be below
 
+@app.route('/')
+def index():
+    return render_template('home.html')
 
+@app.route('/shirt')
+def shirt():
+    return render_template('product_shirt.html')
+
+
+@app.route('/jeans')
+def jeans():
+    return render_template('product_jeans.html')
+
+
+@app.route('/heels')
+def heels():
+    return render_template('product_heels.html')
+
+
+@app.route('/hat')
+def hat():
+    return render_template('product_hat.html')
 
 
 # Your code should be above
